1649d373824f42279cd043400d9cf0430df8fa5c
YashRaut24 <yashcodezzzz24@gmail.com>
2025-10-27 19:09:23 +0530

    Added profile component

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,21 +1,23 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import CreatePost from "./components/CreatePost";
 import ShowPost from "./components/ShowPost";
 import SearchUser from "./components/SearchUser";
 import Click from "./components/Click";
+import Profile from "./components/Profile";
 import "./App.css";
 
 const App = () => {
   const [showCreate, setShowCreate] = useState(false);
   const [showSearchUser, setShowSearchUser] = useState(false);
   const [showClick, setShowClick] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
   const [refreshTrigger, setRefreshTrigger] = useState(0);
-  let [darkMode,setDarkMode] = useState(localStorage.getItem("darkMode") === "true");
+  let [darkMode, setDarkMode] = useState(localStorage.getItem("darkMode") === "true");
 
   function changeTheme() {
     localStorage.setItem("darkMode", !darkMode);
-    setDarkMode((prev)=>!prev);
+    setDarkMode((prev) => !prev);
   }
 
   useEffect(() => {
@@ -32,24 +34,26 @@ const App = () => {
   const refreshPosts = () => setRefreshTrigger((prev) => prev + 1);
   const toggleSearchUser = () => setShowSearchUser((prev) => !prev);
   const toggleClick = () => setShowClick((prev) => !prev);
-  
+  const toggleProfile = () => setShowProfile((prev) => !prev);
 
   return (
-    <div className={darkMode? "dark-app-container" : "app-container"}>
+    <div className={darkMode ? "dark-app-container" : "app-container"}>
       <Navbar 
         mode={darkMode}
         changeTheme={changeTheme}
         toggleCreatePost={toggleCreatePost}
         toggleSearchUser={toggleSearchUser}
         toggleClick={toggleClick}
+        toggleProfile={toggleProfile}
       />
 
       <main>
-        {showCreate && <CreatePost mode = {darkMode} setRefreshTrigger={setRefreshTrigger} />}
-        {showSearchUser && <SearchUser mode = {darkMode}/>}
-        {showClick && <Click mode = {darkMode} onClose={toggleClick} onUpload={refreshPosts} />}
+        {showCreate && <CreatePost mode={darkMode} setRefreshTrigger={setRefreshTrigger} />}
+        {showSearchUser && <SearchUser mode={darkMode} />}
+        {showClick && <Click mode={darkMode} onClose={toggleClick} onUpload={refreshPosts} />}
+        {showProfile && <Profile mode={darkMode} onClose={toggleProfile} />}
 
-        <ShowPost mode = {darkMode} refreshTrigger={refreshTrigger} />
+        <ShowPost mode={darkMode} refreshTrigger={refreshTrigger} />
       </main>
     </div>
   );
