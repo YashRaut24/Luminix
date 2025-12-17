96394a041a6a51726a9301c2ab4e5704b8e4ed36
YashRaut24 <yashcodezzzz24@gmail.com>
2025-12-17 18:26:58 +0530

    add user signin API with password verification

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 router.post("/signup", async (req, res) => {
   const { name, email, password } = req.body;
@@ -10,12 +11,50 @@ router.post("/signup", async (req, res) => {
   }
 
   try {
-    const user = new User({ name, email, password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({ name, email, password : hashedPassword });
     await user.save();
     res.status(201).json({ message: "User registered" });
   } catch (err) {
     res.status(400).json({ message: "Email already exists" });
   }
 });
+
+router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+      message: "Signin successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
 
 module.exports = router;
