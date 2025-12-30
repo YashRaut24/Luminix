4a1e4b94746c42b2bc43bc944b280fd88ed396cc
YashRaut24 <yashcodezzzz24@gmail.com>
2025-12-30 22:00:41 +0530

    Changed the cookie name and fixed bugs

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middleware/auth");
+
 
 router.post("/signup", async (req, res) => {
   const { name, email, password } = req.body;
@@ -19,6 +22,10 @@ router.post("/signup", async (req, res) => {
   } catch (err) {
     res.status(400).json({ message: "Email already exists" });
   }
+});
+
+router.get("/me", authMiddleware, (req, res) => {
+  res.json({ user: req.user });
 });
 
 router.post("/signin", async (req, res) => {
@@ -41,6 +48,19 @@ router.post("/signin", async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      {expiresIn: "1d" }
+    );
+
+    res.cookie("luminix_token", token, {
+      httpOnly: true,
+      secure:false,
+      sameSite: "lax",
+      maxAge: 24 * 60 * 60 * 1000
+    })
+
     res.status(200).json({
       message: "Signin successful",
       user: {
@@ -55,6 +75,9 @@ router.post("/signin", async (req, res) => {
   }
 });
 
-
+router.post("/logout", (req, res) => {
+  res.clearCookie("luminix_token");
+  res.json({ message: "Logged out" });
+});
 
 module.exports = router;
