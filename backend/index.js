607903f35723a50e4831c52be0ba687cdde1e3f8
YashRaut24 <yashcodezzzz24@gmail.com>
2025-12-30 22:00:02 +0530

    Edited cookie name

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const connectDB = require("./config/db.js");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
 const app = express();
+app.use(cookieParser());
 
-app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:5174",
+  credentials: true
+}));
+
 app.use(express.json());
 connectDB();
 
