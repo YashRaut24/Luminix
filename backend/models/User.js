bdd3d8c327adcef6555de06044945eb23b37ac98
YashRaut24 <yashcodezzzz24@gmail.com>
2026-01-04 22:24:32 +0530

    Updated user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
-  password: String
+  password: String,
+
+  profileImage: {
+    type:String,
+    default: "",
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
