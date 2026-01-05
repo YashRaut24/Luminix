83ef5cdcfc207335402809e26e3f92b56e361e7c
YashRaut24 <yashcodezzzz24@gmail.com>
2026-01-05 20:13:56 +0530

    Updated schema for connections pages (added,tags,profileImage,username)

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,11 +4,43 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
-
   profileImage: {
-    type:String,
+    type: String,
     default: "",
   },
+  profileBackground: {
+    type: String,
+    default: "",
+  },
+  lumiTag: {
+    type: String,
+    default: function() {
+      return `@${this.name.toLowerCase().replace(/\s+/g, '')}`;
+    }
+  },
+  role: {
+    type: String,
+    enum: ["creators", "knownPeople", "others"],
+    default: "others"
+  },
+  creatorRole: {
+    type: String,
+    default: ""
+  },
+  bio: {
+    type: String,
+    default: ""
+  },
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }],
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }]
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model("User", userSchema);
